6283b3f103725452dc3c2f244f527683beaeca8a
Miguel Pimiento <mgefrad@gmail.com>
2025-04-16 21:28:28 -0500

    feat(registro de movimientos): movimeinto esn base de datos

---
+++ backend/src/main/java/mytiendita/backend/util/Constantes.java
@@ -0,0 +1,8 @@
+package mytiendita.backend.util;
+
+public class Constantes {
+
+    public static final Long ID_TIPO_MOVIMIENTO_VENTA = 1L;
+    public static final Long ID_TIPO_MOVIMIENTO_COMPRA = 2L;
+    public static final Long ID_TIPO_MOVIMIENTO_SALIDA = 3L;
+}
